13c74c3bd341766ddb790eac3294ae2f222729e5
psytwo1 <psytwo1@gmail.com>
2022-12-09 17:35:59 +0900

    ver 0.0.1 release

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# fake-runner
+# FakeRunner
+FakeRunner uses idevicelocation to send location information written to iOS device triggered by writing to Firestore
+
+# System Requirement
+- idevicelocation
+- Mac
+- iOS device
+  
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+// const USER_ID = "user is here";
+import { USER_ID } from "./user_id";
+
+// const UDID = "device UDID here";
+import { UDID } from "./udid";
+
+import { exec } from "child_process";
+
+import { cert, initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+
+const serviceAccount = require("../sa-key.json");
+
+initializeApp({
+  credential: cert(serviceAccount),
+});
+
+const db = getFirestore();
+
+const doc = db.collection("locations").doc(USER_ID);
+
+const observer = doc.onSnapshot(
+  (docSnapshot: any) => {
+    // console.log(`Received doc snapshot: ${docSnapshot}`);
+    const data = docSnapshot.data();
+    // console.log(`docSpaishot data is ${data}`);
+    const latitude = data.coordinate.latitude;
+    const longitude = data.coordinate.longitude;
+    // console.log(`latitude is ${latitude} longitude is ${longitude}`);
+    const cmdline = `idevicelocation -u ${UDID} -- ${latitude} ${longitude}`;
+    console.log(cmdline);
+    exec(cmdline, (err: any, stdout: any, stderr: any) => {
+      if (err) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`${stdout}`);
+    });
+  },
+  (err: any) => {
+    console.log(`Encountered error: ${err}`);
+  }
+);
